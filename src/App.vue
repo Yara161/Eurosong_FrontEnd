bacaac61ff1c4503a989cb75abf70e16cc77075e
Yara Van Dam <yara@netpulse.be>
2021-11-22 10:48:52 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Eurosong festival app"/>
+    <!-- Navigatie -->
+    <nav class="c-nav">
+      <ul class="c-nav-list">
+        <li class="c-nav-list-item">
+          Home
+        </li>
+
+        <li class="c-nav-list-item">
+          Contact
+        </li>
+      </ul>
+    </nav>
+
+    <h1> Eurosong Festival </h1>
+
+    <div class="c-feedback warning">
+      Er is een fout gebeurd.
+    </div>
+
+    <div class=" c-feedback error">
+    'T is kapot
+    </div>
+    <div class="c-feedback succes">
+      Gelukt
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import of the whole styling for the app
+import style from './scss/style.scss'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
